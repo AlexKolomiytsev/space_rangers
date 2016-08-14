8fedaa34d0f2a69ed93034bdae31eb0db0ce021e
AlexKolomiytsev <kolomiytsev.alex@gmail.com>
2016-08-14 15:22:43 +0300

    model completed

--- resources/assets/js/model/model.js
+++ resources/assets/js/model/model.js
@@ -8,12 +8,12 @@ function Model() {
     this.destroyShips = 0; //destroyed ships
 
     this.spaceships = [
-        {position: ["03", "04", "05"], damage: ["", "", ""], color: "red"},
-        {position: ["11", "21", "31"], damage: ["", "", ""], color: "blue"},
-        {position: ["15", "25", "35"], damage: ["", "", ""], color: "red"},
-        {position: ["26", "36", "46"], damage: ["", "", ""], color: "blue"},
-        {position: ["41", "42", "43"], damage: ["", "", ""], color: "red"},
-        {position: ["52", "53", "54"], damage: ["", "", ""], color: "blue"}
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "red"},
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "blue"},
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "red"},
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "blue"},
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "red"},
+        {position: ["0", "0", "0"], damage: ["", "", ""], color: "blue"}
     ];
 }
 
@@ -25,29 +25,38 @@ Model.prototype.shot = function (id) {
         if (posDamage >= 0) {
 
             if (spaceship.damage[posDamage] === 'loss') {
-                view.showMsg("This ship is already knocked out");
+                //view.showMsg("This ship is already knocked out");
                 return true;
             }
 
             spaceship.damage[posDamage] = "loss";
             var color = spaceship.color;
 
-            view.showShip(id, color);
-            view.showMsg("knockout");
+            //view.showShip(id, color);
+            //view.showMsg("knockout");
 
             if (this.checkDestroyedShip(spaceship)) {
-                view.showMsg("Flotilla of 3 ships is destroyed");
+                //view.showMsg("Flotilla of 3 ships is destroyed");
                 this.destroyShips++;
-                return true;
+                return {
+                    id: id,
+                    color: color,
+                    status: 3
+                }
             }
 
-            return true;
+            return {
+                id: id,
+                color: color,
+                status: 1
+            }
         }
     }
 
-    view.showAsteroid(id);
-    view.showMsg("miss");
-    return false;
+    //view.soundShot();
+    //view.showAsteroid(id);
+    //view.showMsg("miss");
+    return id
 };
 
 Model.prototype.checkDestroyedShip = function (ship) {
@@ -57,6 +66,57 @@ Model.prototype.checkDestroyedShip = function (ship) {
         }
     }
     return true;
+};
+
+Model.prototype.createShipPos = function () {
+    var col = 0;
+    var row = 0;
+    var location = Math.floor(Math.random() * 2);
+    var shipPosition = [];
+
+    if (location === 1) { //horizontal
+        row = Math.floor(Math.random() * this.sizeSpace);
+        col = Math.floor(Math.random() * (this.sizeSpace - this.lengthShips));
+    }
+    else { //vertical
+        row = Math.floor(Math.random() * (this.sizeSpace - this.lengthShips));
+        col = Math.floor(Math.random() * this.sizeSpace);
+    }
+
+    for (var i = 0; i < this.lengthShips; i++) {
+        if (location === 1) {
+            shipPosition.push(row + "" + (col + i));
+        }
+        else {
+            shipPosition.push((row + i) + "" + col);
+        }
+    }
+
+    return shipPosition;
+};
+
+Model.prototype.checkRepeatPos = function (position) {
+    for (var i = 0; i < this.numShips; i++) {
+        var spaceship = this.spaceships[i];
+        for (var j = 0; j < position.length; j++) {
+            if (spaceship.position.indexOf(position[j]) >= 0) {
+                return true;
+            }
+        }
+    }
+    return false;
+};
+
+Model.prototype.createSpaceships = function () {
+    var position;
+
+    for (var i = 0; i < this.numShips; i++) {
+        do {
+            position = this.createShipPos();
+        }
+        while (this.checkRepeatPos(position));
+        this.spaceships[i].position = position;
+    }
 };
 
 
